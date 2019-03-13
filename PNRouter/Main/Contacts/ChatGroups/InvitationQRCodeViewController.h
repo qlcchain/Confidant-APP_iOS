82e936d6d7185d29b48da12e86a5261f83a5cac8
kuangzihui <kuangzihui@163.com>
2019-03-13 14:09:41 +0800

    update

---
+++ PNRouter/Main/Contacts/ChatGroups/InvitationQRCodeViewController.h
@@ -0,0 +1,17 @@
+//
+//  InvitationQRCodeViewController.h
+//  PNRouter
+//
+//  Created by 旷自辉 on 2019/3/13.
+//  Copyright © 2019 旷自辉. All rights reserved.
+//
+
+#import "PNBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InvitationQRCodeViewController : PNBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PNRouter/Main/Contacts/ChatGroups/InvitationQRCodeViewController.m
@@ -0,0 +1,32 @@
+//
+//  InvitationQRCodeViewController.m
+//  PNRouter
+//
+//  Created by 旷自辉 on 2019/3/13.
+//  Copyright © 2019 旷自辉. All rights reserved.
+//
+
+#import "InvitationQRCodeViewController.h"
+
+@interface InvitationQRCodeViewController ()
+
+@end
+
+@implementation InvitationQRCodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
