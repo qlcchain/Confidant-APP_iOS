41a147a95f004512424a90ab919289de325d8b39
QQQOOO <741229443@qq.com>
2019-03-18 14:48:41 +0800

    update group

---
+++ PNRouter/Main/Contacts/ChatGroups/Views/GroupMemberView.m
@@ -0,0 +1,47 @@
+//
+//  GroupMemberView.m
+//  PNRouter
+//
+//  Created by Jelly Foo on 2019/3/18.
+//  Copyright © 2019 旷自辉. All rights reserved.
+//
+
+#import "GroupMemberView.h"
+
+@implementation GroupMemberView
+
++ (instancetype)getInstance {
+    GroupMemberView *view = [[[NSBundle mainBundle] loadNibNamed:@"GroupMemberView" owner:self options:nil] lastObject];
+    return view;
+}
+
+- (IBAction)head1Action:(id)sender {
+    
+}
+
+- (IBAction)head2Action:(id)sender {
+    
+}
+
+- (IBAction)head3Action:(id)sender {
+    
+}
+
+- (IBAction)head4Action:(id)sender {
+    
+}
+
+- (IBAction)head5Action:(id)sender {
+    
+}
+
+- (IBAction)addAction:(id)sender {
+    
+}
+
+- (IBAction)delAction:(id)sender {
+    
+}
+
+
+@end
