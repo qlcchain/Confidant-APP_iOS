be5088b2005fba5d038939d58bef216c9eb3e741
QQQOOO <741229443@qq.com>
2019-03-18 18:20:15 +0800

    update select group member

--- PNRouter/Main/Contacts/ChatGroups/Views/ChooseDownView.h
+++ PNRouter/Main/Contacts/ChatGroups/Views/ChooseDownView.h
@@ -9,9 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ChooseDownView : UIView
+
 @property (weak, nonatomic) IBOutlet UILabel *lblContent;
 @property (weak, nonatomic) IBOutlet UIView *confirmBackView;
 @property (weak, nonatomic) IBOutlet UIButton *comfirmBtn;
 
 + (instancetype) loadChooseDownView;
+
 @end
