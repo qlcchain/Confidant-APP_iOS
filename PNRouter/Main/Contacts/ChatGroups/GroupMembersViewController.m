15fb328887dba4da83148995fa0c32edfdcd8088
kuangzihui <kuangzihui@163.com>
2019-04-03 11:40:03 +0800

    update

--- PNRouter/Main/Contacts/ChatGroups/GroupMembersViewController.m
+++ PNRouter/Main/Contacts/ChatGroups/GroupMembersViewController.m
@@ -19,6 +19,7 @@
 #import "AddGroupMemberViewController.h"
 #import "GroupInfoModel.h"
 #import "FriendDetailViewController.h"
+#import "UserModel.h"
 
 @interface GroupMembersViewController ()<UITextFieldDelegate,UITableViewDelegate,UITableViewDataSource>
 
@@ -120,19 +121,22 @@
     
     @weakify_self
     [view setClickBlock:^(GroupMembersModel * _Nonnull model) {
-       FriendModel *fModel = [[ChatListDataUtil getShareObject] getFriendWithUserid:model.ToxId];
-        if (!fModel) {
-            fModel = [[FriendModel alloc] init];
-            fModel.userId = model.ToxId;
-            fModel.username = [model.Nickname base64DecodedString];
-            fModel.signPublicKey = model.UserKey;
-            fModel.noFriend = YES;
-        } else {
-            fModel.username = fModel.username ?[fModel.username base64DecodedString]:@"";
-            fModel.remarks = fModel.remarks ?[fModel.remarks base64DecodedString]:@"";
-            
+        if (![model.ToxId isEqualToString:[UserModel getUserModel].userId]) {
+            FriendModel *fModel = [[ChatListDataUtil getShareObject] getFriendWithUserid:model.ToxId];
+            if (!fModel) {
+                fModel = [[FriendModel alloc] init];
+                fModel.userId = model.ToxId;
+                fModel.username = [model.Nickname base64DecodedString];
+                fModel.signPublicKey = model.UserKey;
+                fModel.noFriend = YES;
+            } else {
+                fModel.username = fModel.username ?[fModel.username base64DecodedString]:@"";
+                fModel.remarks = fModel.remarks ?[fModel.remarks base64DecodedString]:@"";
+                
+            }
+            [weakSelf jumpFriendDetailVC:fModel];
         }
-        [weakSelf jumpFriendDetailVC:fModel];
+       
     }];
     
     return view;
