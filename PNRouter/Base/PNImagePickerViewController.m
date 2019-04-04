94758e253af82956943c5ffde47292373c787f62
QQQOOO <741229443@qq.com>
2019-04-04 10:43:03 +0800

    update

---
+++ PNRouter/Base/PNImagePickerViewController.m
@@ -0,0 +1,32 @@
+//
+//  PNImagePickerViewController.m
+//  PNRouter
+//
+//  Created by Jelly Foo on 2019/4/4.
+//  Copyright © 2019 旷自辉. All rights reserved.
+//
+
+#import "PNImagePickerController.h"
+
+@interface PNImagePickerController ()
+
+@end
+
+@implementation PNImagePickerController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
