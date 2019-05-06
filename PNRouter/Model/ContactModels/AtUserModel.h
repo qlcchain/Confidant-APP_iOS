26689bfca74c391afc605f4a07dca564968222ca
kuangzihui <kuangzihui@163.com>
2019-05-06 18:07:06 +0800

    Add @ function

---
+++ PNRouter/Model/ContactModels/AtUserModel.h
@@ -0,0 +1,19 @@
+//
+//  AtUserModel.h
+//  PNRouter
+//
+//  Created by 旷自辉 on 2019/5/5.
+//  Copyright © 2019 旷自辉. All rights reserved.
+//
+
+#import "BBaseModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AtUserModel : BBaseModel
+@property (nonatomic, copy) NSString *userId;
+@property (nonatomic, copy) NSString *userName;
+@property (nonatomic, copy) NSString *atName;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PNRouter/Model/ContactModels/AtUserModel.m
@@ -0,0 +1,13 @@
+//
+//  AtUserModel.m
+//  PNRouter
+//
+//  Created by 旷自辉 on 2019/5/5.
+//  Copyright © 2019 旷自辉. All rights reserved.
+//
+
+#import "AtUserModel.h"
+
+@implementation AtUserModel
+
+@end
